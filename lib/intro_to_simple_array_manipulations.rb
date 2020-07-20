eeedd24b695cd0e69a750cbf5166e294d9a203f3
Troy Dawson <troyd9216@gmail.com>
2020-07-20 19:43:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,6 @@ def using_unshift(array, string)
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
 def using_pop(array)
-continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
 continents.pop 
 end
 
