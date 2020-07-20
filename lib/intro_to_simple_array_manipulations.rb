452161ff99a0015fb8b04acbaf15186cbace3ecd
Troy Dawson <troyd9216@gmail.com>
2020-07-20 19:47:50 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,18 +10,15 @@ def using_unshift(array, string)
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
 def using_pop(array)
-continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-continents.pop 
+array.pop 
 end
 
 def pop_with_args(array)
-dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-small_dogs = dog_breeds.pop(2)
+small_dogs = array.pop(2)
 end
 
 def using_shift(array)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = my_favorite_cities.shift
+  im_so_over_this_city = array.shift
 end
 
 def shift_with_args(array)
